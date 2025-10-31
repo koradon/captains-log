d481432c6048e385d930a963ed5e6c7866abf3f2
Michał Sipa <msipa@lulu.com>
2025-10-31 09:44:07 +0100

    Update coverage and linting configuration to exclude auto-generated _version.py file
    - Added exclusion for src/_version.py in coverage settings to avoid unnecessary reporting.
    - Updated ruff configuration to exclude src/_version.py from linting checks.
    - Incremented version in _version.py to '0.1.1.dev2+gdd852f2c2.d20251031' and adjusted version tuple accordingly.

--- src/_version.py
+++ src/_version.py
@@ -12,7 +12,8 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.1.dev1+g1aea1a190.d20251031"
-__version_tuple__ = version_tuple = (0, 1, 1, "dev1", "g1aea1a190.d20251031")
+__version__ = version = '0.1.1.dev2+gdd852f2c2.d20251031'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev2', 'gdd852f2c2.d20251031')
 
 __commit_id__ = commit_id = None
